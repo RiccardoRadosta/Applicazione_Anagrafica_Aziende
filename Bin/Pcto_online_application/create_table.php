c54f5a051e29fd5a444d48ec80c1180d2a9d745a
user <user@gmail.com>
2020-02-13 10:25:06 +0100

    Modified the create_table file

--- Bin/Pcto_online_application/create_table.php
+++ Bin/Pcto_online_application/create_table.php
@@ -7,10 +7,14 @@ $num = $stmt->rowCount();
 if ($num > 0){
   echo("<table>");
   for ($i=0; $i < $num; $i++){
+    $conta = 0;
     $riga = $stmt->fetch(PDO::FETCH_NUM);
     echo "<tr>";
     foreach($riga as $key => $value){
+      $conta = $conta + 1;
+      if (($conta != 7) and ($conta != 8)){
         echo "<td>$value</td>";
+      }
     }
     echo "</tr>";
   }
