ec74176dc86f077a8c447b97ec5b4a3db4c0198e
user <user@gmail.com>
2020-03-07 14:08:47 +0100

    Problem with the DB hosting

--- Bin/Pcto_online_application/db_connect.php
+++ Bin/Pcto_online_application/db_connect.php
@@ -1,24 +1,24 @@
 <?php
 
-#$host = "remotemysql.com";
-#$db_name = "Q1pyVGXE2Q";
-#$username = "Q1pyVGXE2Q";
-#$password = "uv8sQq6Ofc";
+$host = "remotemysql.com";
+$db_name = "Q1pyVGXE2Q";
+$username = "Q1pyVGXE2Q";
+$password = "uv8sQq6Ofc";
 
-#try {
-#    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
-#}
-#catch(PDOException $exception){
-#    echo "<script>console.log(".$exception->getMessage().");</script>";
-#}
-$host = "localhost";
-$db_name = "anagr";
-$username = "root";
-$password = "";
-try{
-  $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
 }
 catch(PDOException $exception){
     echo "<script>console.log(".$exception->getMessage().");</script>";
 }
+#$host = "localhost";
+#$db_name = "anagr";
+#$username = "root";
+#$password = "";
+#try{
+#  $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+#}
+#catch(PDOException $exception){
+#    echo "<script>console.log(".$exception->getMessage().");</script>";
+#}
 ?>
